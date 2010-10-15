09fdddea202adb5290641d496a3a051e10660ead
Jonas Arnklint <jonas@fkw.se>
2010-10-15 08:11:11 +0200

    Context menu items are now links for easier css hover styling

--- jquery.contextMenu.js
+++ jquery.contextMenu.js
@@ -30,14 +30,17 @@ jQuery.fn.contextMenu = function ( name, actions ) {
     $(document).bind("contextmenu",function(e){ return false; }); // yuk :)
     
     var menu = $('<ul id="'+name+'" class="context-menu"></ul>').hide().appendTo('body'),
-        active_element = null; // last clicked element that responds with contextMenu
-
-    var hide_menu = function(){
-        $('.context-menu').hide()
-    }
+        active_element = null, // last clicked element that responds with contextMenu
+        hide_menu = function(){
+            $('.context-menu').hide()
+        };
+    
+    $(".context-menu").delegate("a", "click", function(e){
+        e.preventDefault();
+    });
 
     $.each(actions, function(me, item_options){
-        $('<li class="'+item_options.class+'">'+me+'</li>')
+        $('<li class="'+item_options.class+'"><a href="#">'+me+'</a></li>')
             .appendTo(menu)
             .bind('click', function(e){
                 item_options.click(active_element);
